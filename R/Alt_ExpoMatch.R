9bba399035b60641d5d3bb24b23edf64f7530826
Idrissben <39485963+Idrissben@users.noreply.github.com>
2022-03-07 17:42:58 -0800

    Update Alt_ExpoMatch.R

--- R/Alt_ExpoMatch.R
+++ R/Alt_ExpoMatch.R
@@ -13,22 +13,6 @@
 
 # fit function #1 is Chris's biggest linear combination discrepancy metric
 
-myfit.imb <- function(matches, BM) {
-  # ASSUME
-  # no outcome and no treatment column in BM
-  # BM is scaled to mean=0, sd=1
-
-  # prepare inputs for Chris's metric
-  trt.X <- BM[matches[, 1], ]
-  ctrl.X <- BM[matches[, 2], ]
-  
-  # find the difference between treated and control units
-  # this returns a matrix with element-wise differences
-  t <- trt.X - ctrl.X
-  # sum each column, and square the value & then sum each element
-  return(sum(colSums(t)^2))
-  
-}
 
 Alt_ExpoMatch <- function(Tr,
                                X,
@@ -69,14 +53,37 @@ Alt_ExpoMatch <- function(Tr,
   n.obs <- length(Tr)
   n.var <- ncol(X)
 
+  #Creating the balance Matrix
+  BM <- scale(X)
+
+  
+  #Creating the custom leximin imbalance function
+  myfit.imb <- function(matches, BM) {
+  # ASSUME
+  # no outcome and no treatment column in BM
+  # BM is scaled to mean=0, sd=1
+
+  # prepare inputs for Chris's metric
+  trt.X <- BM[matches[, 1], ]
+  ctrl.X <- BM[matches[, 2], ]
+  
+  # find the difference between treated and control units
+  # this returns a matrix with element-wise differences
+  t <- trt.X - ctrl.X
+  # sum each column, and square the value & then sum each element
+  return(sum(colSums(t)^2))
+  
+}
+
   if (start.gm) {
     # run an initial GenMatch to get starting parameters
     genout.start <- GenMatch(Tr = Tr,
                              X = X,
                              pop.size = pop.size*50,
                              max.generations = max.generations*2,
-                             BalanceMatrix = X,
-                             hard.generation.limit = TRUE)
+                             BalanceMatrix = BM,
+                             hard.generation.limit = TRUE,
+                             fit.func = myfit.imb)
     print("The initial GenMatch run determined the following variable weights:")
     print(genout.start$par)
     start.weights <- genout.start$par
@@ -89,7 +96,11 @@ Alt_ExpoMatch <- function(Tr,
 
   # prepare domains
   dom <- cbind(rep(domains[1], n.var), rep(domains[2], n.var))
+  
+  
 
+  
+  
   GenMatchWrapper <- function(exponents) {
 
     if (print.level == 2) {
@@ -97,7 +108,8 @@ Alt_ExpoMatch <- function(Tr,
     }
 
     XN <- X
-
+    
+    
     for (i in c(1:n.var)) {
       XN[, i] <- XN[, i]^exponents[i]
     }
@@ -108,8 +120,9 @@ Alt_ExpoMatch <- function(Tr,
                        project.path = paste(tempdir(), "/genoud.txt", sep = ""),
                        pop.size = pop.size,
                        max.generations = max.generations,
-                       BalanceMatrix = X,
-                       starting.values = start.weights)
+                       BalanceMatrix = BM,
+                       starting.values = start.weights,
+                       fit.func = myfit.imb)
 
     return(genout$value[1]) # = highest lowest p-value
   }
@@ -141,8 +154,9 @@ Alt_ExpoMatch <- function(Tr,
                          X = XM,
                          pop.size = pop.size*50,
                          max.generations = max.generations*2,
-                         BalanceMatrix = X,
-                         starting.values = best.weights)
+                         BalanceMatrix = BM,
+                         starting.values = best.weights,
+                         fit.func = myfit.imb)
   mout.fin <- Match(Tr = Tr, X = XM, Weight.matrix = genout.fin)
 
   # prepare output
