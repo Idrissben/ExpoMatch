ef10fa741552c20e86fcbb03f1d5d57ecfcca457
Idrissben <39485963+Idrissben@users.noreply.github.com>
2022-03-09 16:47:27 -0800

    Delete get_estimate.R

--- R/get_estimate.R
+++
@@ -1,6 +0,0 @@
-#This function is aimed at getting the causal estimate of the matches provided (currently supports ATT only)
-
-get_estimate <- function(Tr = Tr, X = X, Y = Y, matches = matches,Expo = TRUE, Expo_weights = NULL) {
-
-  if (Expo){
-
